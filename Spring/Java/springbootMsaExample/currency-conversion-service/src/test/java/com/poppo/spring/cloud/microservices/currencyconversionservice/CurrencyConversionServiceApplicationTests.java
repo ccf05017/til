5aa8b72ff7c7f18d57cc376bdeef4299d1ffe39d
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-01-14 09:22:59 +0900

    Currency Conversion Service
    - 프로젝트 설정

---
+++ Spring/Java/springbootMsaExample/currency-conversion-service/src/test/java/com/poppo/spring/cloud/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poppo.spring.cloud.microservices.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
