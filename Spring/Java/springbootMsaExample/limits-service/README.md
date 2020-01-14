8bbc6a6007639d0f12d859f3ca6a215b01104a8f
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-01-14 23:46:44 +0900

    Fault Tolerance
    - Hystrix 의존성 추가
    - 에러 발생 시 동작할 기본 fallback 함수 생성
    - HystrixCommand를 통해 fallback 함수 연결

--- Spring/Java/springbootMsaExample/limits-service/README.md
+++ Spring/Java/springbootMsaExample/limits-service/README.md
@@ -3,3 +3,5 @@
 - Web, Actuator, Config Client 의존성이 있다.
 - 기본적으로 8888 포트에 컨피그 받기 요청을 보낸다.
 - 없다면 현재 bootstrap.yml 파일을 바라본다.
+- config 설정이 바뀌면 새로고침을 진행해야 바뀐 내용이 반영된다.
+- POST {host}/actuator/refresh
--- Spring/Java/springbootMsaExample/limits-service/src/main/java/com/poppo/spring/cloud/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ Spring/Java/springbootMsaExample/limits-service/src/main/java/com/poppo/spring/cloud/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package com.poppo.spring.cloud.microservices.limitsservice.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.poppo.spring.cloud.microservices.limitsservice.YmlConfigurations;
 import com.poppo.spring.cloud.microservices.limitsservice.domain.LimitConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,5 +17,18 @@ public class LimitsConfigurationController {
     public LimitConfiguration retrieveLimitsFromConfigurations() {
 
         return new LimitConfiguration(ymlConfigurations.getMaximum(), ymlConfigurations.getMinimum());
+    }
+
+    @GetMapping("/fault-tolerance-example")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+    public LimitConfiguration retrieveConfiguration() {
+
+        throw new RuntimeException("Not Available");
+    }
+
+    public LimitConfiguration fallbackRetrieveConfiguration() {
+
+        // 문제가 있을 때 최소한의 기능으로 동작하도록 조치를 취함
+        return new LimitConfiguration(9, 999);
     }
  }
