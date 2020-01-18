cce2ec41d2f8818e114f5a4172ab86b94dfee944
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-01-19 00:05:50 +0900

    테스트용 서버 코드 작성
    - TCP 50051에서 서버 시작하는 코드 작성
    - 서버 프로세스가 전체를 블록하지 않도록 설정(server.awaitTermination())
    - 서버 끄기 hook 추가

---
+++ grpc/src/main/java/com/poppo/grpc/server/GreetingServer.java
@@ -0,0 +1,31 @@
+package com.poppo.grpc.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GreetingServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        System.out.println("Hello gRPC! Server on 50051");
+
+        Server server = ServerBuilder.forPort(50051)
+                .build();
+
+        server.start();
+
+        // 꺼지는 조건 추가
+        // 중요
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Received Shutdown Request");
+            server.shutdown();
+            System.out.println("Successfully Stopped The Server");
+        }));
+
+        // 이건 뭐여..?
+        // 이걸 안하면 서버 프로세스가 전체 블로킹 건다는 듯..?
+        server.awaitTermination();
+    }
+}
