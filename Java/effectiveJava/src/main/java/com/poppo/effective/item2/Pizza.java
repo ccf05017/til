ad2f789cca9a5c6a2d4b7e4db954a0fdbc9f3114
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-01-28 14:33:10 +0900

    item2. 빌더 패턴을 사용해보자

---
+++ Java/effectiveJava/src/main/java/com/poppo/effective/item2/Pizza.java
@@ -0,0 +1,27 @@
+package com.poppo.effective.item2;
+
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
+
+public abstract class Pizza {
+    public enum Topping { HAM, MUSHROOM, ONION, PEPPER, SAUSAGE }
+    final Set<Topping> toppings;
+
+    abstract static class Builder<T extends Builder<T>> {
+        EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
+        public T addTopping(Topping topping) {
+            toppings.add(Objects.requireNonNull(topping));
+            return self();
+        }
+
+        abstract Pizza build();
+
+        protected abstract T self();
+    }
+
+    Pizza(Builder<?> builder) {
+        // 사본을 입력
+        this.toppings = builder.toppings.clone();
+    }
+}
