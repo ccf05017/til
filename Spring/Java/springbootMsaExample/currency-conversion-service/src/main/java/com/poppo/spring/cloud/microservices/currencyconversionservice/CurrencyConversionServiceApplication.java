311e3ef6237c4fe75052d3f1013e8d6476eef14e
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-02-15 23:41:18 +0900

    Spring Cloud 인증, 인가 기능 실습
    - Zuul Server 설정 변경: 토큰 전달을 위해 Sensitive Header 설정 변경
    - ConversionService Resource 서버 지정: 보호하기 위해 Resource 서버 지정 및 Security 관련 컨텍스트 등 구현
    - OAuth 인증 서버 구축: 인증 엔드 포인트 및 토큰 발행 프로세스 구현

--- Spring/Java/springbootMsaExample/currency-conversion-service/src/main/java/com/poppo/spring/cloud/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ Spring/Java/springbootMsaExample/currency-conversion-service/src/main/java/com/poppo/spring/cloud/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -1,15 +1,20 @@
 package com.poppo.spring.cloud.microservices.currencyconversionservice;
 
 import brave.sampler.Sampler;
+import com.poppo.spring.cloud.microservices.currencyconversionservice.filter.UserContextFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import javax.servlet.Filter;
 
 @SpringBootApplication
 @EnableFeignClients("com.poppo.spring.cloud.microservices.currencyconversionservice")
 @EnableDiscoveryClient
+@EnableResourceServer
 public class CurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
@@ -20,5 +25,11 @@ public class CurrencyConversionServiceApplication {
     public Sampler defaultSampler() {
 
         return Sampler.ALWAYS_SAMPLE;
+    }
+
+    @Bean
+    public Filter userContextFilter() {
+
+        return new UserContextFilter();
     }
 }
---
+++ Spring/Java/springbootMsaExample/spring-cloud-security/src/main/java/com/poppo/spring/cloud/security/config/WebSecurityConfigurer.java
@@ -0,0 +1,38 @@
+package com.poppo.spring.cloud.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    @Primary
+    @Bean
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth
+                .inMemoryAuthentication()
+                .passwordEncoder(encoder)
+                .withUser("glovis").password(encoder.encode("kisa")).roles("USER")
+                .and()
+                .withUser("hae").password(encoder.encode("kisa")).roles("USER", "ADMIN");
+    }
+}
