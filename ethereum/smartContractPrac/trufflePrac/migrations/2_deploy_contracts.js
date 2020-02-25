9cf5035c1b7c351bef60d7c0b97ee4d8abdf01b9
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-02-26 08:54:55 +0900

    트러플 실습

--- ethereum/smartContractPrac/trufflePrac/migrations/2_deploy_contracts.js
+++ ethereum/smartContractPrac/trufflePrac/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const Voting = artifacts.require("./Voting.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Voting, ['poopo', 'ita', 'saul']);
+  deployer.deploy(Voting, ['poopo', 'ita', 'saul'].map(name => web3.utils.asciiToHex(name)));
 };
