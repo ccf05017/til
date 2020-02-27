a449ecf7edd7f90f713742a4189a6f00e833cc81
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-02-28 08:01:38 +0900

    트러플 실습

--- ethereum/smartContractPrac/tokenPrac/contracts/Voting.sol
+++ ethereum/smartContractPrac/tokenPrac/contracts/Voting.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.0 <0.6.0;
 contract Voting {
   struct voter {
     address voterAddress;
-    uint tokenBought;
+    uint tokensBought;
     uint[] tokenUsedPerCandidate;
   }
 
@@ -17,10 +17,18 @@ contract Voting {
   uint public balanceTokens;
   uint public tokenPrice;
 
-  constructor(uint tokens, uint pricePerToken, bytes32[] memory candidateNames) public {
+  constructor (uint tokens, uint pricePerToken, bytes32[] memory candidateNames) public {
     candidateList = candidateNames;
     totalTokens = tokens;
     balanceTokens = tokens;
     tokenPrice = pricePerToken;
+  }
+
+  function buy() payable public {
+    uint tokensToBuy = msg.value / tokenPrice;
+    require(tokensToBuy <= balanceTokens);
+    voterInfo[msg.sender].voterAddress = msg.sender;
+    voterInfo[msg.sender].tokensBought += tokensToBuy;
+    balanceTokens -= tokensToBuy;
   }
 }
