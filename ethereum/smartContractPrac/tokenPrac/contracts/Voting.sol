40abb9d78b3b503d0ab57b004646d147a360d050
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-02-28 12:36:42 +0900

    트러플 실습
    - 토큰 앱 투표 기능 구현

--- ethereum/smartContractPrac/tokenPrac/contracts/Voting.sol
+++ ethereum/smartContractPrac/tokenPrac/contracts/Voting.sol
@@ -4,7 +4,7 @@ contract Voting {
   struct voter {
     address voterAddress;
     uint tokensBought;
-    uint[] tokenUsedPerCandidate;
+    uint[] tokensUsedPerCandidate;
   }
 
   mapping (address => voter) voterInfo;
@@ -30,5 +30,53 @@ contract Voting {
     voterInfo[msg.sender].voterAddress = msg.sender;
     voterInfo[msg.sender].tokensBought += tokensToBuy;
     balanceTokens -= tokensToBuy;
+  }
+  function voteForCandidate(bytes32 candidate, uint tokens) public {
+    uint availableTokens = voterInfo[msg.sender].tokensBought - totalTokensUsed(voterInfo[msg.sender].tokensUsedPerCandidate);
+
+    require(tokens <= availableTokens, "You don't have enough tokens");
+    votesReceived[candidate] += tokens;
+
+    if(voterInfo[msg.sender].tokensUsedPerCandidate.length == 0) {
+      for (uint i = 0; i < candidateList.length; i++) {
+        voterInfo[msg.sender].tokensUsedPerCandidate.push(0);
+      }
+    }
+
+    uint index = indexOfCandidate(candidate);
+    voterInfo[msg.sender].tokensUsedPerCandidate[index] += tokens;
+  }
+
+  function indexOfCandidate(bytes32 candidate) view public returns(uint) {
+    for(uint i = 0; i <candidateList.length; i++) {
+      if (candidateList[i] == candidate) {
+        return i;
+      }
+    }
+    return uint(-1);
+  }
+
+  function totalTokensUsed(uint[] memory _tokensUsedPerCandidate) private pure returns (uint) {
+    uint totalUsedTokens = 0;
+    for(uint i = 0; i < _tokensUsedPerCandidate.length; i++) {
+      totalUsedTokens += _tokensUsedPerCandidate[i];
+    }
+    return totalUsedTokens;
+  }
+
+  function voterDetails(address user) view public returns (uint, uint[] memory) {
+    return (voterInfo[user].tokensBought, voterInfo[user].tokensUsedPerCandidate);
+  }
+
+  function tokensSold() public view returns (uint) {
+    return totalTokens - balanceTokens;
+  }
+
+  function allCandidates() public view returns(bytes32[] memory) {
+    return candidateList;
+  }
+
+  function totalVotesFor(bytes32 candidate) public view returns (uint) {
+    return votesReceived[candidate];
   }
 }
--- ethereum/smartContractPrac/tokenPrac/migrations/2_deploy_contracts.js
+++ ethereum/smartContractPrac/tokenPrac/migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+const Voting = artifacts.require("Voting");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(Voting, 10000, web3.utils.toWei('0.01', 'ether'), ['Rama', 'Nick', 'Jose'].map(name => web3.utils.asciiToHex(name)));
 };
