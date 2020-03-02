0024c4c071d0b71411e4ea26dc10ff0af934a219
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-03-03 08:24:33 +0900

    리액트 네이티브 실습 앱

---
+++ Mobile/ReactNative/smoothieBook/FirstApp/App.js
@@ -0,0 +1,87 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import Styled from 'styled-components/native';
+
+const ScrollView = Styled.ScrollView`
+  backgroud-color: ${Colors.lighter};
+`;
+
+const Body = Styled.View`
+  backgroud-color: ${Colors.white};
+`;
+
+const SectionContainer = Styled.View`
+  margin-top: 32px;
+  padding-horizontal: 24px;
+`;
+
+const SectionDescription = Styled.Text`
+  margin-top: 8px;
+  font-size: 18px;
+  font-weight: 400;
+  color: ${Colors.dark};
+`;
+
+const HighLight = Styled.Text`
+  font-weight: 700;
+`;
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+          <Header />
+          <Body>
+            <SectionContainer>
+              <SectionDescription>Step One</SectionDescription>
+              <SectionDescription>
+                Edit <HighLight>App.js</HighLight> to change this screen and
+                then come back to see your edits.
+              </SectionDescription>
+            </SectionContainer>
+            <SectionContainer>
+              <SectionDescription>See Your Changes</SectionDescription>
+              <SectionDescription>
+                <ReloadInstructions />
+              </SectionDescription>
+            </SectionContainer>
+            <SectionContainer>
+              <SectionDescription>Debug</SectionDescription>
+              <SectionDescription>
+                <DebugInstructions />
+              </SectionDescription>
+            </SectionContainer>
+            <SectionContainer>
+              <SectionDescription>Lean More</SectionDescription>
+              <SectionDescription>
+                Read the docs to discover what to do next:
+              </SectionDescription>
+            </SectionContainer>
+            <LearnMoreLinks />
+          </Body>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
