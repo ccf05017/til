10f8b87dd9b93f454a4debe2b0ad87d19ed54073
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-04-01 14:38:29 +0900

    안드로이드 개발 학습
    - Fragment 개념 학습 및 실습

---
+++ android/practise/app/src/main/java/com/poppo/practise/fragment/FragmentActivity.kt
@@ -0,0 +1,70 @@
+package com.poppo.practise.fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.FragmentManager
+import com.poppo.practise.R
+import kotlinx.android.synthetic.main.activity_fragment.*
+
+class FragmentActivity : AppCompatActivity(), FragmentOne.OnDataPassListener {
+
+    override fun onDataPass(data: String?) {
+        Log.d("pass", data)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment)
+
+        val fragmentOne : FragmentOne = FragmentOne()
+        val bundle : Bundle = Bundle()
+        bundle.putString("hello", "hello")
+        fragmentOne.arguments = bundle
+
+        button.setOnClickListener {
+            val fragmentManager : FragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.container, fragmentOne)
+            fragmentTransaction.commit()
+        }
+
+        button2.setOnClickListener {
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction();
+            // fragmentTransaction.detach(fragmentOne)
+            fragmentTransaction.remove(fragmentOne)
+            fragmentTransaction.commit()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d("life_cycle", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("life_cycle", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d("life_cycle", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d("life_cycle", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d("life_cycle", "onDestroy")
+    }
+}
