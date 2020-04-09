31d6e31c724e1d332b76a493a78cf215de25e866
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-04-10 08:25:33 +0900

    안드로이드 학습
    - Pager, TabLayout 실습

---
+++ android/practise/app/src/main/java/com/poppo/practise/TabPagerActivity.kt
@@ -0,0 +1,63 @@
+package com.poppo.practise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.google.android.material.tabs.TabLayout
+import com.poppo.practise.fragment.Fragment2
+import com.poppo.practise.fragment.Fragment3
+import com.poppo.practise.fragment.Fragment4
+import kotlinx.android.synthetic.main.activity_tab_pager.*
+
+class TabPagerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tab_pager)
+
+        tab_layout.addTab(tab_layout.newTab().setText("one"))
+        tab_layout.addTab(tab_layout.newTab().setText("two"))
+        tab_layout.addTab(tab_layout.newTab().setText("three"))
+
+        val pagerAdapter = PagerAdapter(supportFragmentManager, 3)
+        view_pager.adapter = pagerAdapter
+
+        tab_layout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                view_pager.currentItem = tab!!.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+            }
+        })
+    }
+}
+
+class PagerAdapter(
+    fragmentManager: FragmentManager,
+    val tabCount: Int
+): FragmentStatePagerAdapter(fragmentManager) {
+    override fun getItem(position: Int): Fragment {
+        when(position) {
+            0 -> {
+                return Fragment2()
+            }
+            1 -> {
+                return Fragment3()
+            }
+            2 -> {
+                return Fragment4()
+            }
+            else -> return Fragment2()
+        }
+    }
+
+    override fun getCount(): Int {
+        return tabCount
+    }
+}
