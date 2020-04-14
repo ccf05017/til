f9a848d94543eda9b4ae73ba02ac03609af3a5b6
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-04-14 15:33:56 +0900

    안드로이드 학습
    - 인스타그램 클론 해보기
    - 로그인, 회원가입 기능 구현
    - 로그인 된 상태에서 글 목록 조회 기능 구현

--- android/practise/app/src/main/java/com/poppo/practise/RetrofitService.kt
+++ android/practise/app/src/main/java/com/poppo/practise/RetrofitService.kt
@@ -1,9 +1,11 @@
 package com.poppo.practise
 
+import com.poppo.practise.instaclone.dto.LoginResponseDto
+import com.poppo.practise.instaclone.dto.PostResponseDto
+import com.poppo.practise.instaclone.dto.RegisterRequestDto
+import com.poppo.practise.instaclone.dto.RegisterResponseDto
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface RetrofitService {
     @GET("json/students/")
@@ -18,4 +20,22 @@ interface RetrofitService {
     fun createStudentEasy(
         @Body person: PersonFromServer
     ): Call<PersonFromServer>
+
+    @POST("user/signup/")
+    @FormUrlEncoded
+    fun register(
+        @Field("username") username: String,
+        @Field("password1") password1: String,
+        @Field("password2") password2: String
+    ): Call<RegisterResponseDto>
+
+    @POST("user/login/")
+    @FormUrlEncoded
+    fun login(
+        @Field("username") username: String,
+        @Field("password") password: String
+    ): Call<LoginResponseDto>
+
+    @GET("instagram/post/list/all")
+    fun getAllPosts(): Call<ArrayList<PostResponseDto>>
 }
---
+++ android/practise/app/src/main/java/com/poppo/practise/instaclone/dto/LoginResponseDto.kt
@@ -0,0 +1,7 @@
+package com.poppo.practise.instaclone.dto
+
+import java.io.Serializable
+
+class LoginResponseDto(
+    var token: String? = null
+): Serializable
