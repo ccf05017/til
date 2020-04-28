a23fc75519979c242b2174e25205827864a3c16c
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-04-28 22:05:36 +0900

    리액트 학습
    - 리액트에서 input을 어떻게 다루는지 개념 학습 및 실습
    - input이 여러개인 경우 어떻게 관리하는지 학습

--- React/react-tutorial/begin-react/src/App.js
+++ React/react-tutorial/begin-react/src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import Hello from './Hello.js';
-import Wrapper from "./Wrapper";
+import MultiInputSample from "./MultiInputSample";
 
 function App() {
   return (
-      <Wrapper>
-        <Hello name="react" color="red" isSpecial={ true }/>
-        <Hello color="pink" />
-      </Wrapper>
+      <MultiInputSample/>
   );
 }
 
