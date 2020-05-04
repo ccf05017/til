fb9bec266afe590fa66ca47441838ca97904dc5a
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-04 11:27:31 +0900

    리액트 학습
    - 배열의 특정 요소 업데이트하기 학습
    - 이것도 배열에 요소를 추가할 때와 마찬가지로 map을 사용하면 된다.

--- React/react-tutorial/begin-react/src/UserList.js
+++ React/react-tutorial/begin-react/src/UserList.js
@@ -3,24 +3,32 @@ const _ = require("fxjs/Strict");
 const L = require("fxjs/Lazy");
 const C = require("fxjs/Concurrency");
 
-function User({ user, onRemove }) {
-    const { username, email, id } = user;
+function User({ user, onRemove, onToggle }) {
+    const { username, email, id, active } = user;
 
     return (
         <div>
-            <b>{username}</b><span>({email})</span>
+            <b style={{
+                    color: active ? 'green' : 'black',
+                    cursor: 'pointer'
+                }}
+               onClick={_ => onToggle(id)}
+            >
+                {username}
+            </b>
+            <span>({email})</span>
             {/* 아래 코드는 콜백으로 '함수의 실행'을 전달 */}
             <button onClick={_ => onRemove(id)}>삭제</button>
         </div>
     );
 }
 
-function UserList({ users, onRemove }) {
+function UserList({ users, onRemove, onToggle }) {
     return (
         <div>
             {
                 users.map(
-                    user => (<User user={user} key={user.id} onRemove={onRemove}/>)
+                    user => (<User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle}/>)
                 )
             }
             {/*{*/}
