c6c1c4fcb78249985bc4c200610710ae7dbbb888
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-09 22:32:49 +0900

    리액트 학습
    - Todo 헤드 기능 구현
    - JS를 이용한 날짜 및 요일 표기
    - Context를 이용한 남은 할일 목록 갯수 구하기

--- React/mashup-todolist/src/App.js
+++ React/mashup-todolist/src/App.js
@@ -4,6 +4,7 @@ import TodoTemplate from "./Components/TodoTemplate";
 import TodoHead from "./Components/TodoHead";
 import TodoList from "./Components/TodoList";
 import TodoCreate from "./Components/TodoCreate";
+import {TodoProvider} from "./Components/TodoContext";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -14,12 +15,14 @@ const GlobalStyle = createGlobalStyle`
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <TodoTemplate>
-        <TodoHead />
-        <TodoList />
-        <TodoCreate />
-      </TodoTemplate>
+      <TodoProvider>
+        <GlobalStyle />
+        <TodoTemplate>
+          <TodoHead />
+          <TodoList />
+          <TodoCreate />
+        </TodoTemplate>
+      </TodoProvider>
     </>
   );
 }
