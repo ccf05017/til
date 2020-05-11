594a82abd8123f0b8db9677b52c4723675afd64a
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-11 13:56:26 +0900

    리액트 학습
    - 리액트에서 url query param 처리하는 방법 학습 및 실습

--- React/react-router/src/About.js
+++ React/react-router/src/About.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import qs from 'qs';
 
-function About() {
+function About({ location }) {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true
+  });
+
+  // query로 불러온 인자는 모두 문자열 타입이다.
+  const detail = query.detail === 'true';
+
   return (
     <div>
       <h1>Introduction</h1>
       <p>
         This page is about practicing react router
       </p>
+      {detail && <p>detail is true</p>}
     </div>
   )
 }
