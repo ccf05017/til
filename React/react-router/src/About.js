cfe7e2de44277cf5f8b64202e34af7f331890a54
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-11 13:41:38 +0900

    리액트 학습
    - SPA를 위한 리액트 라우터 학습 및 실습
    - Browser Router(HTML5 이용)

---
+++ React/react-router/src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>Introduction</h1>
+      <p>
+        This page is about practicing react router
+      </p>
+    </div>
+  )
+}
+
+export default About;
