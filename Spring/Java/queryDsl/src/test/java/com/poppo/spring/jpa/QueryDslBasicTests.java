ba647c1ae45edf414f81819268bf2071f1614a4a
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-05-13 11:35:02 +0900

    QueryDSL groupby 학습
    - QueryDSL 자체적인 tuple을 결과값으로 사용함
    - groupby에 제한을 줘야 한다면 having 메서드로 제한할 수 있다

--- Spring/Java/queryDsl/src/test/java/com/poppo/spring/jpa/QueryDslBasicTests.java
+++ Spring/Java/queryDsl/src/test/java/com/poppo/spring/jpa/QueryDslBasicTests.java
@@ -3,6 +3,7 @@ package com.poppo.spring.jpa;
 import com.poppo.spring.jpa.entity.Member;
 import com.poppo.spring.jpa.entity.Team;
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,11 +12,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
-
 import java.util.List;
 
+import static com.poppo.spring.jpa.entity.QMember.member;
+import static com.poppo.spring.jpa.entity.QTeam.team;
 import static org.assertj.core.api.Assertions.assertThat;
-import static com.poppo.spring.jpa.entity.QMember.*;
 
 @SpringBootTest
 @Transactional
@@ -115,5 +116,49 @@ public class QueryDslBasicTests {
         assertThat(memberQueryResults.getLimit()).isEqualTo(2);
         assertThat(memberQueryResults.getOffset()).isEqualTo(1);
         assertThat(memberQueryResults.getResults().size()).isEqualTo(2);
+    }
+
+    @Test
+    public void aggregation() {
+        List<Tuple> result = jpaQueryFactory
+                .select(
+                        member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min()
+                )
+                .from(member)
+                .fetch();
+
+        // QueryDSL tuple
+        // 데이터가 단일 타입이 아닐 경우에 쓴다.
+        // 많이는 안씀
+        Tuple tuple = result.get(0);
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+        assertThat(tuple.get(member.age.max())).isEqualTo(40);
+        assertThat(tuple.get(member.age.min())).isEqualTo(10);
+    }
+
+    @Test
+    public void group() throws Exception {
+        List<Tuple> result = jpaQueryFactory
+                .select(team.name, member.age.avg())
+                .from(member)
+                .join(member.team, team)
+                .groupBy(team.name)
+                .fetch();
+
+        Tuple teamA = result.get(0);
+        Tuple teamB = result.get(1);
+
+        assertThat(teamA.get(team.name)).isEqualTo("teamA");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+        assertThat(teamB.get(team.name)).isEqualTo("teamB");
+        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+
+        // groupBy된 결과에 제한 값을 주고 싶으면 having()
     }
 }
