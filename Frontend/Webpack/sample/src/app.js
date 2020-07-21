01f62801cf50a37037844494acdf3d0679261217
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-21 09:10:44 +0900

    merge

--- Frontend/Webpack/sample/src/app.js
+++ Frontend/Webpack/sample/src/app.js
@@ -1,11 +1,1 @@
-import './app.css';
-import nyancat from './nyancat.jpg';
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.body.innerHTML = `
-    <img src="${nyancat}" />
-  `
-});
-
-console.log(process.env.NODE_ENV);
-console.log(TWO);
+new Promise();
