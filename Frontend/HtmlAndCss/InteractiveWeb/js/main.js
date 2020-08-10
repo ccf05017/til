409f9d6b6271ed175a57d85f822456ac1d8e8770
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-08-10 11:19:10 +0900

    반응형 웹 기존 수업 내용 업로드

---
+++ Frontend/HtmlAndCss/InteractiveWeb/js/main.js
@@ -0,0 +1,49 @@
+(() => {
+  const sceneInfo = [
+    {
+      type: 'sticky',
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-0')
+      }
+    },
+    {
+      type: 'normal',
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-1')
+      }
+    },
+    {
+      type: 'sticky',
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-2')
+      }
+    },
+    {
+      type: 'sticky',
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-3')
+      }
+    }
+  ];
+
+  function setLayout() {
+    sceneInfo.forEach((scene) => {
+      scene.scrollHeight = scene.heightNum * window.innerHeight;
+      scene.objs.container.style.height = `${scene.scrollHeight}px`;
+    })
+
+    console.log(sceneInfo);
+  }
+
+  window.addEventListener('resize', setLayout);
+  
+  setLayout();
+})();
