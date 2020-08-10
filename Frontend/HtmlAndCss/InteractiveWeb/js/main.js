291fa470b9ad0d8de56628ad71c8d0b86a32055c
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-08-10 13:03:46 +0900

    feat: track current scene
    - 현재 스크롤 y 위치를 기준으로 scene을 얼마나 지나왔는지 추적
    - 이 추적값과 현재 y 위치를 비교해서 활성화 된 scene 번호 결정

--- Frontend/HtmlAndCss/InteractiveWeb/js/main.js
+++ Frontend/HtmlAndCss/InteractiveWeb/js/main.js
@@ -1,4 +1,8 @@
 (() => {
+  let currentYOffset = 0;   // 현재 y offset의 값
+  let sumOfpreviousSceneY = 0;    // 이전 scene들의 y 높이의 합
+  let currentScene = 0;   // 현재 보여줘야 할 scene 번호
+
   const sceneInfo = [
     {
       type: 'sticky',
@@ -38,12 +42,31 @@
     sceneInfo.forEach((scene) => {
       scene.scrollHeight = scene.heightNum * window.innerHeight;
       scene.objs.container.style.height = `${scene.scrollHeight}px`;
-    })
+    });
+  }
 
-    console.log(sceneInfo);
+  function scrollLoop() {
+    sumOfpreviousSceneY = 0;
+    for (let i = 0; i < currentScene; i++) {
+      sumOfpreviousSceneY += sceneInfo[i].scrollHeight;
+    }
+
+    if (currentYOffset > sumOfpreviousSceneY + sceneInfo[currentScene].scrollHeight) {
+      currentScene++;
+    }
+
+    if (currentYOffset < sumOfpreviousSceneY) {
+      currentScene--;
+    }
+
+    console.log(currentScene);
   }
 
   window.addEventListener('resize', setLayout);
+  window.addEventListener('scroll', () => {
+    currentYOffset = window.pageYOffset;     // 스크롤이 움직일 때마다 현재의 y 좌표를 추적함.
+    scrollLoop();
+  });
   
   setLayout();
 })();
