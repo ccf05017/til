861bf5cccdae4c30a8acc8a8996300e673287ef9
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-08-26 14:03:25 +0900

    study(QueryDSL): paging, sorting
    - QueryDSL을 이용한 페이징, 정렬 처리를 어떻게 하는지 학습

--- Backend/Spring/Java/queryDsl/src/test/java/com/poppo/querydsl/QueryDslBasicTests.java
+++ Backend/Spring/Java/queryDsl/src/test/java/com/poppo/querydsl/QueryDslBasicTests.java
@@ -1,8 +1,8 @@
 package com.poppo.querydsl;
 
 import com.poppo.querydsl.entity.Member;
-import com.poppo.querydsl.entity.QMember;
 import com.poppo.querydsl.entity.Team;
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,8 +11,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
+import static com.poppo.querydsl.entity.QMember.member;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @Transactional
@@ -20,8 +23,12 @@ public class QueryDslBasicTests {
     @PersistenceContext
     EntityManager entityManager;
 
+    JPAQueryFactory jpaQueryFactory;
+
     @BeforeEach
     public void setup() {
+        jpaQueryFactory = new JPAQueryFactory(entityManager);
+
         Team team = new Team("teamA");
         Team teamB = new Team("teamB");
 
@@ -53,15 +60,123 @@ public class QueryDslBasicTests {
 
     @Test
     public void queryDslExample() {
-        JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
-        QMember m = new QMember("m");
+        // QMember m = new QMember("m");
+        // QMember m = QMember.member; // 이걸 static import로 바꿔서 사용하는 방법도 있다.
 
-        Member foundMember = queryFactory
-                .select(m)
-                .from(m)
-                .where(m.username.eq("member1"))
+        Member foundMember = jpaQueryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
                 .fetchOne();
 
         assertThat(foundMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void searchExample() {
+        Member foundMember = jpaQueryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.eq(10)))
+                .fetchOne();
+
+        assertThat(foundMember.getUsername()).isEqualTo("member1");
+
+        Member notFoundMember = jpaQueryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.eq(11)))
+                .fetchOne();
+
+        assertThatThrownBy(() -> notFoundMember.getUsername()).isInstanceOf(NullPointerException.class);
+    }
+
+    // where 절에 파라미터 형태로 조건을 던져서 and 쿼리르 작성할 수 있다.(or는 안된다)
+    @Test
+    public void searchAndParam() {
+        Member foundMember = jpaQueryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                )
+                .fetchOne();
+
+        assertThat(foundMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void getResultTest() {
+//        List<Member> members = jpaQueryFactory
+//                .selectFrom(member)
+//                .fetch();
+//
+//        Member fetchOne = jpaQueryFactory
+//                .selectFrom(QMember.member)
+//                .fetchOne();
+//
+//        Member fetchFirst = jpaQueryFactory
+//                .selectFrom(QMember.member)
+//                .fetchFirst(); // limit(1).fetchOne()
+
+        QueryResults<Member> results = jpaQueryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+        long total = results.getTotal();
+        List<Member> content = results.getResults();
+        assertThat(total).isEqualTo(4);
+        assertThat(content.get(0).getUsername()).isEqualTo("member1");
+
+        long totalCount = jpaQueryFactory
+                .selectFrom(member)
+                .fetchCount();
+        assertThat(totalCount).isEqualTo(4);
+    }
+
+    @Test
+    public void sort() {
+        // 나이 내림차순
+        // 이름 올림차순
+        // 이름이 없으면 마지막에 출력
+        entityManager.persist(new Member(null, 100));
+        entityManager.persist(new Member("member5", 100));
+        entityManager.persist(new Member("member6", 100));
+
+        List<Member> members = jpaQueryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        assertThat(members.get(0).getUsername()).isEqualTo("member5");
+        assertThat(members.get(1).getUsername()).isEqualTo("member6");
+        assertThat(members.get(2).getUsername()).isNull();
+    }
+
+    @Test
+    public void pagingTest() {
+        List<Member> members = jpaQueryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetch();
+
+        assertThat(members.size()).isEqualTo(2);
+        assertThat(members.get(0).getUsername()).isEqualTo("member3");
+    }
+
+    @Test
+    public void pagingWithCountTest() {
+        QueryResults<Member> memberQueryResults = jpaQueryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetchResults();
+
+        assertThat(memberQueryResults.getTotal()).isEqualTo(4);
+        assertThat(memberQueryResults.getResults().get(0).getUsername()).isEqualTo("member3");
     }
 }
