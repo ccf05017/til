c0492ef3cb9184ff16bb4a53b36e7965a0a8ed01
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-08-27 09:15:01 +0900

    study(EffectiveJava): use composite!
    - 왜 상속보다 컴포지션을 써야 되는지에 대해 학습
    - 컴포지션과 Wrpper 클래스를 활용하면 재사용성이 얼마나 좋아지는지 학습

---
+++ Language/Java/effectiveJava/src/main/java/com/poppo/effective/item18/InstrumentedSet.java
@@ -0,0 +1,28 @@
+package com.poppo.effective.item18;
+
+import java.util.Collection;
+import java.util.Set;
+
+public class InstrumentedSet<E> extends WrapperSet<E> {
+    private int addCount = 0;
+
+    public InstrumentedSet(Set<E> set) {
+        super(set);
+    }
+
+    @Override
+    public boolean add(E e) {
+        addCount++;
+        return super.add(e);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        addCount += c.size();
+        return super.addAll(c);
+    }
+
+    public int getAddCount() {
+        return addCount;
+    }
+}
