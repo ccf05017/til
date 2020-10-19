24d4359d31b2c507b27567f9d4bb8939553c2b28
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-10-19 10:57:30 +0900

    refactor(CalculateSample): apply template pattern
    - 템플릿 콜백 패턴 적용을 통해 코드 재사용성 향상
    - 곱하기 기능 추가

--- Backend/Spring/Java/toby/src/main/java/com/poppo/toby/simpleSamples/CalculatorSample.java
+++ Backend/Spring/Java/toby/src/main/java/com/poppo/toby/simpleSamples/CalculatorSample.java
@@ -6,15 +6,47 @@ import java.io.IOException;
 
 public class CalculatorSample {
     public int calcSum(String path) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
-        int sum = 0;
-        String line = null;
+        return this.fileReadTemplate(path, (BufferedReader bufferedReader) -> {
+            int sum = 0;
+            String line = null;
 
-        while((line = bufferedReader.readLine()) != null) {
-            sum += Integer.parseInt(line);
+            while((line = bufferedReader.readLine()) != null) {
+                sum += Integer.parseInt(line);
+            }
+
+            return sum;
+        });
+    }
+
+    public int calcMultiply(String path) throws IOException {
+        return this.fileReadTemplate(path, (BufferedReader bufferedReader) -> {
+            int sum = 1;
+            String line = null;
+
+            while((line = bufferedReader.readLine()) != null) {
+                sum *= Integer.parseInt(line);
+            }
+
+            return sum;
+        });
+    }
+
+    public int fileReadTemplate(String filePath, BufferedReaderCallback bufferedReaderCallback) throws IOException {
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(filePath));
+            return bufferedReaderCallback.doSomethingWithReader(bufferedReader);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
         }
-
-        bufferedReader.close();
-        return sum;
     }
 }
--- Backend/Spring/Java/toby/src/test/java/com/poppo/toby/simpleSamples/CalculatorSampleTest.java
+++ Backend/Spring/Java/toby/src/test/java/com/poppo/toby/simpleSamples/CalculatorSampleTest.java
@@ -1,20 +1,56 @@
 package com.poppo.toby.simpleSamples;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.util.ResourceUtils;
 
 import java.io.IOException;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CalculatorSampleTest {
-    @Test
-    void addTest() throws IOException {
-        int expectedResult = 10;
+    @ParameterizedTest
+    @MethodSource("addTestSuccessResources")
+    void addTestSuccess(int expectedResult, String resourcePath) throws IOException {
         CalculatorSample calculatorSample = new CalculatorSample();
-        
-        int sum = calculatorSample.calcSum(ResourceUtils.getFile("classpath:numbers.txt").getPath());
+
+        int sum = calculatorSample.calcSum(ResourceUtils.getFile(resourcePath).getPath());
 
         assertThat(sum).isEqualTo(expectedResult);
+    }
+    public static Stream<Arguments> addTestSuccessResources() {
+        return Stream.of(
+                Arguments.of(10, "classpath:numbers.txt"),
+                Arguments.of(14, "classpath:numbers2.txt")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("multiplyTestSuccessResources")
+    void multiplyTestSuccess(int expectedResult, String resourcePath) throws IOException {
+        CalculatorSample calculatorSample = new CalculatorSample();
+
+        int sum = calculatorSample.calcMultiply(ResourceUtils.getFile(resourcePath).getPath());
+
+        assertThat(sum).isEqualTo(expectedResult);
+    }
+    public static Stream<Arguments> multiplyTestSuccessResources() {
+        return Stream.of(
+                Arguments.of(24, "classpath:numbers.txt"),
+                Arguments.of(120, "classpath:numbers2.txt")
+        );
+    }
+
+    @Test
+    void addTestFail() {
+        String notExistResource = "notExist";
+        CalculatorSample calculatorSample = new CalculatorSample();
+
+        assertThatThrownBy(() -> calculatorSample.calcSum(ResourceUtils.getFile(notExistResource).getPath()))
+                .isInstanceOf(IOException.class);
     }
 }
