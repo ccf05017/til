607c45b05ac2cc717548b44bea79d46a1661a5fa
poppohoo <ccf05017@gmail.com>
2021-02-23 09:23:18 +0900

    practis(Spring): apply spring dynamic proxy to transaction
    - 트랜잭션 기능에 다이내믹 프록시 적용하기 실습

--- Backend/Spring/Java/toby/src/test/java/com/poppo/toby/TestBeanConfiguration.java
+++ Backend/Spring/Java/toby/src/test/java/com/poppo/toby/TestBeanConfiguration.java
@@ -3,6 +3,9 @@ package com.poppo.toby;
 import com.poppo.toby.services.*;
 import com.poppo.toby.userDao.UserDao;
 import com.poppo.toby.userDao.UserDaoJdbc;
+import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -15,13 +18,32 @@ import javax.sql.DataSource;
 @Configuration
 public class TestBeanConfiguration {
     @Bean(name = "userService")
-    public TxProxyFactoryBean txProxyFactoryBean() {
-        return new TxProxyFactoryBean(transactionManager(), userServiceImpl(), "upgradeLevels", UserService.class);
+    public ProxyFactoryBean txProxyFactoryBean() {
+        ProxyFactoryBean proxyFactoryBean = new ProxyFactoryBean();
+        proxyFactoryBean.addAdvisor(transactionAdvisor());
+        return proxyFactoryBean;
     }
 
     @Bean
     public Object userService() throws Exception {
         return txProxyFactoryBean().getObject();
+    }
+
+    @Bean
+    public TransactionAdvice transactionAdvice() {
+        return new TransactionAdvice(transactionManager());
+    }
+
+    @Bean
+    public NameMatchMethodPointcut transactionPointcut() {
+        NameMatchMethodPointcut nameMatchMethodPointcut = new NameMatchMethodPointcut();
+        nameMatchMethodPointcut.addMethodName("upgrade*");
+        return nameMatchMethodPointcut;
+    }
+
+    @Bean
+    public DefaultPointcutAdvisor transactionAdvisor() {
+        return new DefaultPointcutAdvisor(transactionPointcut(), transactionAdvice());
     }
 
     @Bean
